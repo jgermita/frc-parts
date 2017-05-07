7d293b404e49d36cb3523caa91a50245ab39a433
Jeremy Paul Germita <jeremy.germita@gmail.com>
2017-05-06 23:59:48 -0700

    scraped data test.

---
+++ scraper/src/scraper/Part.java
@@ -0,0 +1,37 @@
+package scraper;
+
+
+public class Part {
+
+	public String	cat, subcat, name, desc, price, pn, vendor, link, id;
+	String			tags[];
+	
+	public Part(String in) {
+
+		String split[] = in.split("\t");
+
+		cat = split[0];
+		subcat = split[1];
+		name = split[2];
+		desc = split[3];
+		price = split[4];
+		pn = split[5];
+		vendor = split[6];
+		link = split[7];
+		id = split[8];
+
+	}
+
+	public String generateMarkdown() {
+
+		String md = "";
+
+		md = md.concat("#" + name + "\n");
+		md = md.concat("##" + cat + "/" + subcat + "\n");
+		md = md.concat(desc + "\n");
+
+		return md;
+
+	}
+
+}
---
+++ scraper/src/scraper/scraper.java
@@ -0,0 +1,54 @@
+package scraper;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+
+public class scraper {
+
+	public static void main(String[] args) {
+
+		String url = "https://docs.google.com/spreadsheets/d/1x07PQ0yxtrQXogLbfGt5_W_RXgm1O1TL-T5Hijq8WTM/pub?gid=0&single=true&output=tsv";
+		
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
+
+			String s = "";
+			while ((s = br.readLine()) != null) {
+				// System.out.println(s);
+				Part p = new Part(s);
+				if (p.id.isEmpty() || p.name.isEmpty()) {
+					continue;
+				}
+				// System.out.println(p.id + ": " + p.name);
+				System.out.println(p.generateMarkdown());
+				writeToFile((p.id + ".md"), p.generateMarkdown());
+				break;
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public static void writeToFile(String fname, String contents) {
+
+		fname = "parts/".concat(fname);
+		File out = new File(fname);
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(out));
+			writer.write(contents);
+			writer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
