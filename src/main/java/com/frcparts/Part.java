a606f4da133103f4a740e58323e94660305c253b
Jeremy Paul Germita <jeremy.germita@gmail.com>
2017-11-23 20:57:18 -0800

    tags

--- src/main/java/com/frcparts/Part.java
+++ src/main/java/com/frcparts/Part.java
@@ -32,7 +32,11 @@ public class Part {
 			url		= params[7];
 			id		= params[8];
 			
-			System.arraycopy(params, 9, tags, 0, params.length - 9);
+                        if(params.length > 9) {
+                            System.arraycopy(params, 9, tags, 0, params.length - 9);
+                        } else {
+                            tags = new String[0];
+                        }
                         
 		}
 	}
@@ -84,11 +88,11 @@ public class Part {
 	
 	public String toString() {
                 String answer = getCat() + "\t" + getSubcat() + "\t" + getName() + "\t" + getDesc() + "\t" + getCost() + "\t" + getPn() + "\t" + getVendor() + "\t" + getUrlLink() + "\t" + getId() + "\t";
-//                if(tags != null) {
-//                    for(String s : tags) {
-//                        answer = answer + ", ";
-//                    }
-//                }
+                if(tags != null) {
+                    for(String s : tags) {
+                        answer = answer + ", ";
+                    }
+                }
 		return answer;
 	}
 
