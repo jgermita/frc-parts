5183c8a0cf661ea00b8190b2374ca56f04c29ec2
Jeremy Paul Germita <jeremy.germita@gmail.com>
2017-11-25 18:13:45 -0800

    s

--- src/main/java/com/frcparts/Part.java
+++ src/main/java/com/frcparts/Part.java
@@ -96,7 +96,7 @@ public class Part {
 	public String toString() {
                 String answer = getCat() + "\t" + getSubcat() + "\t" + getName() + "\t" + getDesc() + "\t" + getCost() + "\t" + getPn() + "\t" + getVendor() + "\t" + getUrlLink() + "\t" + getId() + "\t";
 
-                if(!s.isEmpty()) {
+                if(tags != null) {
                     for(String s : tags) {
 
                         answer = answer + s + ", ";
