04f9f05729af37ea64304589a0e6b96081b9badf
Jeremy Paul Germita <jeremy.germita@gmail.com>
2017-11-26 22:34:49 -0800

    small description

--- src/main/java/com/frcparts/Part.java
+++ src/main/java/com/frcparts/Part.java
@@ -135,7 +135,7 @@ public class Part {
 	}
 	
 	public String toString() {
-                String answer = getCat() + "\t" + getSubcat() + "\t<a href=\"part.jsp?p=" + getId() + "\">" + getName() + "</a>\t" + getDesc() + "\t" + getUrlLink() + "\t" + getSourceLink() + "\t";
+                String answer = getCat() + "\t" + getSubcat() + "\t<a href=\"part.jsp?p=" + getId() + "\">" + getName() + "</a>\t<small>" + getDesc() + "</small>\t" + getUrlLink() + "\t" + getSourceLink() + "\t";
 
                 if(tags != null) {
                     for(String s : tags) {
