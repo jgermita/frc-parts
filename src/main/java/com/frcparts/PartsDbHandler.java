50720351bd448e1001022e2eaaa0391b6fd657b5
Jeremy Paul Germita <jeremy.germita@gmail.com>
2017-11-29 01:07:37 -0800

    maybe faster?

--- src/main/java/com/frcparts/PartsDbHandler.java
+++ src/main/java/com/frcparts/PartsDbHandler.java
@@ -18,106 +18,111 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class PartsDbHandler {
     public SheetsData data = null;
-    
+
     String results;
     String query;
     String category = "electrical";
 	public Part searched;
-    
+
     public PartsDbHandler() {
-        data = new SheetsData(new SheetsFetcher("https://docs.google.com/spreadsheets/d/e/2PACX-1vTBWVWKUQK8WMiqk0m34zccY3wuvS7QmmkSZXZtQ_5gh675xN4zslR2vOdPpftjLVFZIpg9WqY55XGX/pub?gid=1184988672&single=true&output=tsv"));
-        searched = data.empty;
-    } 
-    
+        init();
+    }
+
+    @PostConstruct
+    public void init() {
+      data = new SheetsData(new SheetsFetcher("https://docs.google.com/spreadsheets/d/e/2PACX-1vTBWVWKUQK8WMiqk0m34zccY3wuvS7QmmkSZXZtQ_5gh675xN4zslR2vOdPpftjLVFZIpg9WqY55XGX/pub?gid=1184988672&single=true&output=tsv"));
+      searched = data.empty;
+    }
+
     public String getSearchResults() {
-        
+
         return this.toString(data.search(query));
     }
-    
+
     public String getCategory(String cat) {
         this.setCategory(cat);
         return this.toTable(data.getCategory(category));
         //return "test";
     }
-    
-    
+
+
     public void setCategory(String cat) {
-        
+
         this.category = cat;
     }
-    
+
     public String getCategory() {
         return this.category;
     }
-    
+
     public void setResults(String results) {
-        
+
         this.results = results;
     }
-    
+
     public void setQuery(String id) {
-        
+
         this.query = id.trim();
-        
+
     }
-    
-    
-    
+
+
+
     public String getQuery() {
         return query;
     }
-    
+
     public String getIds() {
         String answer = "";
-        
+
         for(Part p : data.getSearchResults()) {
             answer = answer + p.getId() + "\n";
         }
-        
+
         return answer;
     }
-    
+
     public String toTable() {
-        
+
         return HtmlHelper.toTable(data.search(query));
     }
-    
+
     public String toTable(ArrayList<Part> p) {
-        
+
         return HtmlHelper.toTable(p);
     }
     public String toString(ArrayList<Part> res) {
-        
+
         String answer = "";
-        
+
         for(Part p : res) {
             answer = answer + p.toString() + " <br>\n";
         }
-        
+
         if(res.size() == 0 || res.get(0).equals(data.empty)) {
             answer = "Nothing found!";
         }
-        
-        
-        
+
+
+
         return answer;
     }
-	
-	
+
+
 	public void searchPart(String id) {
 		for(Part p : data.cache) {
 			if(p.getId().equals(id)) {
 				searched = p;
 			}
 		}
-		
+
 	}
-    
-    
+
+
     public String getCacheTimestamp() {
         return data.getCacheTimestamp();
     }
-    
+
     public String getSearchListings() {
         ArrayList<Part> results = data.search(query);
         String answer = "<section class=\"post\">";
@@ -133,14 +138,14 @@ public class PartsDbHandler {
 					answer = answer + "<div class=\"post-description\"><p>" + p.getDesc() + "</p></div><br>\n";
 				}
 			}
-            
+
             answer = answer + "\n";
         }
-        
+
         answer = answer + "</section>";
-        
+
         return answer;
-        
+
     }
-    
+
 }
